7904b2f0fccaff636ed1817d08c8a6b096824e33
Zach Gawlik <zachgawlik@gmail.com>
2015-02-12 15:20:16 -0500

    Fix timing bug when tab inactive

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(function() {
     var BREAK_PERIOD = 5 * 60;
     var isBreak = false;
     var isPaused = false;
-    var minutes, seconds, leadHex, hex;
     var timer = WORK_PERIOD;
     var maxTime = WORK_PERIOD;
     var $time = $('#time');
@@ -13,34 +12,50 @@ $(function() {
         $('#progressbar').width(100 * timer / maxTime + '%');
     }
 
-    function updateTime() {
-        seconds = timer % 60;
-        minutes = (timer - seconds) / 60;
-
+    function humanizeTimer() {
+        var seconds = timer % 60;
+        var minutes = (timer - seconds) / 60;
         if (minutes < 10) { minutes = '0' + minutes; }
         if (seconds < 10) { seconds = '0' + seconds; }
+        return {minutes: minutes, seconds: seconds}
+    }
 
+    function updateDisplayTime(remaining) {
+        $time.text(remaining.minutes + ' : ' + remaining.seconds);
+    }
+
+    function updateDisplayColor(remaining) {
         leadHex = isBreak ? '#BB' : '#00';
-        hex = leadHex + minutes + seconds; 
-
-        updateProgressbar();
-        
+        hex = leadHex + remaining.minutes + remaining.seconds; 
         $color.text(hex);
-        $time.text(minutes + ' : ' + seconds);
         $('body').css('background-color', hex);
+    }
 
-        timer -= 1;
+    var prev = new Date();
+    function updateTime() {
+        var now = new Date();
+        timer -= Math.round((now - prev) / 1000);
+        remaining = humanizeTimer(timer)
+
+        updateDisplayTime(remaining);
+        updateDisplayColor(remaining);
+        updateProgressbar();
+        prev = new Date();
     }
 
     $('body').click(function() {
         isPaused = !isPaused;
+        if (!isPaused) {
+            prev = new Date();
+        }
         $('.pause-text').toggle();
         $('.text').toggle();
     });
 
+    var startTime = new Date();
     setInterval(function() {
         if (isPaused) return;
-        if (!timer) {
+        if (timer <= 0) {
             isBreak = !isBreak;
             maxTime = isBreak ? BREAK_PERIOD : WORK_PERIOD;
             timer = maxTime;
